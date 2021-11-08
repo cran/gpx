b3d9a95d1e767ce3f97253c511c548a2c80aa1a6
Mark Ewing <b.mark@ewingsonline.com>
2021-11-08 15:40:02 +0000

    version 1.1.0

--- R/helpers.R
+++ R/helpers.R
@@ -7,6 +7,7 @@ extract_tracks = function(data) {
   } else {
     output = lapply(points, process_points)
     output = lapply(seq_along(output), function(i){output[[i]][["Segment ID"]] = i;return(output[[i]])}) #nolint
+    names(output) = vapply(tracks, extract_feature, NA_character_, "name")
   }
   return(output)
 }
@@ -18,6 +19,16 @@ extract_routes = function(data) {
     output = generate_empty()
   } else {
     output = lapply(points, process_points)
+  }
+  return(output)
+}
+
+extract_waypoints = function(data) {
+  points = rvest::html_nodes(data, "wpt")
+  if (length(points) == 0) {
+    output = generate_empty()
+  } else {
+    output = process_waypoints(points)
   }
   return(output)
 }
@@ -65,6 +76,25 @@ extract_extensions = function(points) {
     output = lapply(output, unlist)
     output = do.call(rbind, output)
   }
+  return(output)
+}
+
+process_waypoints = function(points) {
+  attrs = rvest::html_attrs(points)
+  df = do.call(rbind, attrs)
+  ele = as.numeric(extract_feature(points, "ele"))
+  time = lubridate::as_datetime(extract_feature(points, "time"))
+  lat = as.numeric(extract_feature(points, "lat"))
+  lon = as.numeric(extract_feature(points, "lon"))
+  name = extract_feature(points, "name")
+  desc = extract_feature(points, "desc")
+  output = data.frame("Elevation" = ele
+                      , "Time" = time
+                      , "Latitude" = lat
+                      , "Longitude" = lon
+                      , "Name" = name
+                      , "Description" = desc
+                      , stringsAsFactors = FALSE)
   return(output)
 }
 
--- tests/testthat/test-gpx.R
+++ tests/testthat/test-gpx.R
@@ -1,10 +1,11 @@
 test_that("basic functionality", {
   expect_error(read_gpx("testdata/fake"))
   expect_equal(class(greasy_test <- read_gpx("testdata/greasy-creek-to-roan-high-knob.gpx")), "list") #nolint
-  expect_length(greasy_test, 2)
+  expect_length(greasy_test, 3)
   expect_length(greasy_test$tracks, 1)
   expect_length(greasy_test$routes, 1)
-  expect_named(greasy_test, c("routes", "tracks"))
+  expect_length(greasy_test$waypoints, 1)
+  expect_named(greasy_test, c("routes", "tracks", "waypoints"))
   expect_equal(nrow(greasy_test$tracks[[1]]), 0)
   expect_equal(ncol(greasy_test$tracks[[1]]), 4)
   expect_equal(nrow(greasy_test$routes[[1]]), 563)
@@ -14,4 +15,7 @@ test_that("basic functionality", {
   expect_true(all(is.numeric(greasy_test$routes[[1]][["Elevation"]])))
   expect_true(all(is.numeric(greasy_test$routes[[1]][["Latitude"]])))
   expect_true(all(is.numeric(greasy_test$routes[[1]][["Longitude"]])))
+
+  expect_equal(class(track_name <- read_gpx("testdata/Bayes_Mountain_Fire_Tower_TN.gpx")), "list") #nolint
+  expect_named(track_name$tracks, "Kingsport Hiking")
 })
